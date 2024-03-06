7318ab8089d6353cce09f3f1b1294da44eace517
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-03-06 14:39:21 +0200

    add tailwind css prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1 className="font-ubuntu  text-3xl font-bold underline">
-        A modern publishing platform
-      </h1>
-      <h1 className="font-overpass  text-3xl font-bold underline">
-        A modern publishing platform
-      </h1>
-      <h1 className="text-3xl font-bold underline">
-        A modern publishing platform
-      </h1>
-      <p className="font-overpass text-Very_light_red_CTA_hover_background">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet optio
-        natus odit ipsam sit illum fuga iste sunt vitae expedita ut perferendis
-        tempora officiis beatae hic ipsa tenetur veritatis, in numquam corporis
-        possimus eligendi. Dolore necessitatibus, sit beatae iusto, aliquam et
-        rerum distinctio asperiores in quam architecto corrupti veritatis sint
-        voluptatibus natus fugiat blanditiis. Quas, doloribus voluptas eligendi
-        harum eum natus quae sequi ipsum aperiam soluta enim. Quam obcaecati
-        reprehenderit earum quo iste hic totam exercitationem harum modi illo
-        reiciendis cumque labore dolorum tempore deserunt incidunt excepturi
-        neque accusantium rerum eveniet a, alias expedita quisquam explicabo.
-        Amet beatae enim nisi repudiandae, sint aut dolorem nulla fugiat est
-        voluptate. Asperiores beatae ipsa earum velit, a laboriosam odit, nihil
-        aliquid eaque tempora assumenda ex dolorem voluptatem magni delectus
-        rem? Totam, alias expedita vitae dicta voluptate consectetur harum.
-        Impedit, alias accusamus autem aliquam iure laborum pariatur. Sequi
-        quisquam ipsum tempore corporis. Reprehenderit, corrupti porro
-        recusandae nostrum quod molestiae possimus mollitia odit, debitis in
-        ipsam ut reiciendis quae quo perferendis delectus molestias aperiam
-        corporis autem voluptas dicta voluptates. Reiciendis, maiores nostrum?
-        Culpa deserunt accusantium commodi impedit optio debitis incidunt,
-        itaque delectus pariatur illum neque, est quia ab, quas fugiat
-        distinctio assumenda nemo nam similique?
-      </p>
-    </>
-  );
+  return <main></main>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,5 +27,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
