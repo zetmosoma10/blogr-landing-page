7791831ad2658330b34a762a00efdc01a660cdce
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-03-08 19:13:11 +0200

    remove usless files and code

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import Feature from "./Sections/Feature";
 import Footer from "./Sections/Footer";
 import Showcase from "./Sections/Showcase";
 import Infrastructure from "./Sections/Infrastructure";
-import Navbar from "./Components/Navbar";
 import Hero from "./Sections/Hero";
 
 function App() {
   return (
-    <main className=" ">
+    <main>
       <Hero />
       <div className="mt-20 lg:mt-36 mb-80 lg:mb-0">
         <Feature />
--- src/Components/DropdownMenu.jsx
+++
@@ -1,5 +0,0 @@
-const DropdownMenu = () => {
-  return <div>DropdownMenu</div>;
-};
-
-export default DropdownMenu;
